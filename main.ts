f83948c019aa5d63bfa49bb0fc29763b785d2cb5
redhead18 <79486286+redhead18@users.noreply.github.com>
2021-03-08 13:24:37 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # . # #
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # # # .
+        . . . . .
+        `)
+})
